eead4849837006500a9122dc36dddebf56cb4eb3
flych3r <xavier_sampaio@alu.ufc.br>
2021-01-13 22:54:55 -0300

    added only total size to du, added hidden folders to ls

--- atividades/atividade05/infoDir.sh
+++ atividades/atividade05/infoDir.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
 if [ -d $1 ]; then
-    SIZE=$(du $1 | cut -f1)
-    N=$(ls $1 | wc -l)
+    SIZE=$(du -s $1 | cut -f1)
+    N=$(ls -a $1 | wc -l)
     echo O diretório $1 ocupa $SIZE kilobytes e tem $N itens.
 else
     echo $1 não é um diretório!!!
