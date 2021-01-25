5b20269f71260b7d9d1206d6d44259c853e59f55
flych3r <xavier_sampaio@alu.ufc.br>
2021-01-25 19:12:57 -0300

    atividade 9

---
+++ atividades/atividade09/alertaDiretorio.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+INTERVAL=$1
+DIRECTORY=$2
+
+LAST=$(ls $DIRECTORY)
+while true; do
+    sleep $INTERVAL
+    DATETIME=$(date +'%d-%m-%Y %H:%M:%S')
+    CURRENT=$(ls $DIRECTORY)
+    if [ "$CURRENT" != "$LAST" ]; then
+        N_LAST=$(wc -l <(echo $LAST | tr ' ' "\n") | cut -d " " -f1)
+        N_CURR=$(wc -l <(echo $CURRENT | tr ' ' "\n") | cut -d " " -f1)
+        N_ADD=$(grep -xvFf <(echo $LAST | tr ' ' "\n") <(echo $CURRENT | tr ' ' "\n"))
+        N_REM=$(grep -xvFf <(echo $CURRENT | tr ' ' "\n") <(echo $LAST | tr ' ' "\n"))
+        if [ -n "$N_ADD" ]; then N_ADD="Adicionados: $(echo $N_ADD | tr "\n" " ") "; fi
+        if [ -n "$N_REM" ]; then N_REM="Removidos: $(echo $N_REM | tr "\n" " ") "; fi
+        echo "[$DATETIME] Alteração! $N_LAST->$N_CURR. $N_ADD$N_REM" | tee -a dirSensors.log
+    fi
+    LAST=$CURRENT
+done
