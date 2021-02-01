32d036270fa2b742cb6b470858747ece2c3b1694
flych3r <xavier_sampaio@alu.ufc.br>
2021-02-01 19:26:11 -0300

    atividade10

---
+++ atividades/atividade10/contaPalavras.sh
@@ -0,0 +1,8 @@
+read -p 'Informe o arquivo: ' FILE
+
+BOW=$(cat $FILE | tr [:punct:] ' ' | tr '[:space:]' '[\n*]' | grep -v "^\s*$" | sort | uniq -c | sort -r | sed 's/  //g' | sed 's/ /:/g')
+for w in $BOW
+do
+    word=(${w//:/ })
+    echo ${word[1]}:$'\t'${word[0]}
+done
